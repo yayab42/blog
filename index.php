dd683bd1858125e1ae1b1ed52ed39c48c8fb6bc2
yayab42 <yannis.bounia@outlook.com>
2021-01-25 15:38:22 +0100

    Etape 1.4

--- index.php
+++ index.php
@@ -11,26 +11,26 @@ if (!isset($_SESSION['dateFirstVisit'])) {
 
 
 $routes = array(
-    "" => '',
+    "index" => '/app/controllers/homeController.php',
     "" => '',
     "" => '',
     "" => '',
     "404" => 'pages/404.php',
 );
 
-$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+$page = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
 
 
 
 if (isset($action)) {
-    if (array_key_exists($action, $routes)) {
-        $route = $routes[$action];
+    if (array_key_exists($page, $routes)) {
+        $route = $routes[$page];
     } else {
         header("HTTP/1.0 404 Not Found");
         $route = $routes['404'];
     }
 } else {
-    $route = $routes[''];
+    $route = $routes['index'];
 }
 include 'config/database.php';
-
+require 'app/controllers/homeController.php';
