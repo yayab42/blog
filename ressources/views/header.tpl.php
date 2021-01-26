6ab34bc60d102024e573c17a0738fc3111d57f3f
yayab42 <yannis.bounia@outlook.com>
2021-01-26 13:38:04 +0100

    Etape 1.6

--- ressources/views/header.tpl.php
+++ ressources/views/header.tpl.php
@@ -1,9 +1,1 @@
 <?php
-$lastPosts = lastBlogPosts($pdo);
-
-
-if (!empty($lastPosts)){
-    var_dump($lastPosts);
-} else {
-    echo "Il n'y a pas de données à afficher";
-}
--- ressources/views/home.tpl.php
+++ ressources/views/home.tpl.php
@@ -1,4 +1,12 @@
 <?php
+$lastPosts = lastBlogPosts($pdo);
+
+
+if (!empty($lastPosts)){
+    var_dump($lastPosts);
+} else {
+    echo "Il n'y a pas de données à afficher";
+}
 require 'ressources/views/header.tpl.php';
 require 'ressources/views/footer.tpl.php';
 
